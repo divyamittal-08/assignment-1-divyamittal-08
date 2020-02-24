eeb1f976c0295d5c9b02914998ff1fc5a970ad2f
Divya Mittal <divyamittal790@gmail.com>
2020-02-24 15:28:37 +0530

    created fields , constructor and getter setters

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,5 +1,48 @@
 package person;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 public class Person {
+    private String fName;
+    private String lName;
+    private ArrayList<String> arr = new ArrayList<>();
+    private String email;
 
+    public Person(String fName, String lName, ArrayList<String> arr, String email){
+        this.fName = fName;
+        this.lName = lName;
+        this.arr = arr;
+        this.email = email;
+    }
+
+    public void setFName(String fName){
+        this.fName = fName;
+    }
+
+    public void setLName(String lName) {
+        this.lName = lName;
+    }
+
+    public void setArr(ArrayList<String> arr) {
+        this.arr = arr;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFName() {
+        return fName;
+    }
+
+    public String getLName() {
+        return lName;
+    }
+
+    public ArrayList<String> getArr() {
+        return arr;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
